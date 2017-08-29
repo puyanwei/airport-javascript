c2f7d1d90e493bd160c945a7eb851549ec3c4608
pyan83 <puyanwei@gmail.com>
2017-08-29 12:54:29 +0100

    Wrote a test for landing a plane

--- jasmine-standalone-2.8.0/spec/airportSpec.js
+++ jasmine-standalone-2.8.0/spec/airportSpec.js
@@ -2,20 +2,11 @@ beforeEach(function() {
   airport = new Airport();
 })
 
-describe('#Airport', function() {
-  it('Jasmine works', function() {
-    expect().toEqual();
+describe('Airport', function() {
+  describe("#landing", function() {
+    it('#landing', function() {
+      airport.landing
+      expect(hanger).toEqual(hanger.length(1));
+    })
   })
 })
-//
-// describe('knows when number is NOT', function() {
-//   it('divisible by 3', function() {
-//     expect(fizzbuzz.says(1)).toEqual(1);
-//   })
-// })
-//
-// describe('when playing, says', function() {
-//   it('"Fizzbuzz" when a number is divisible by 15', function() {
-//     expect(fizzbuzz.says(15)).toEqual("Fizzbuzz")
-//   })
-// })
