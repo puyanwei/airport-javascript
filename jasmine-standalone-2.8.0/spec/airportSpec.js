cc8de77b70c20988dba4611b5ccbc2e5f4b914c1
pyan83 <puyanwei@gmail.com>
2017-08-30 00:23:27 +0100

    add throws to airport, but tests failing

--- jasmine-standalone-2.8.0/spec/airportSpec.js
+++ jasmine-standalone-2.8.0/spec/airportSpec.js
@@ -1,12 +1,46 @@
-beforeEach(function() {
-  airport = new Airport();
-})
-
 describe('Airport', function() {
+  var airport;
+  var plane;
+
+  beforeEach(function() {
+    airport = new Airport();
+    plane = new Plane();
+  })
+
+  describe("#initialize", function() {
+    it("hanger is empty at the start", function() {
+      expect(airport.hanger.length).toEqual(0);
+    })
+  })
+
   describe("#landing", function() {
     it("hanger has one plane when landing", function() {
-      airport.landing();
+      airport.landing(plane);
       expect(airport.hanger.length).toEqual(1);
+    })
+  })
+
+  describe("#landing", function() {
+    it("plane cannot land if hanger is full", function() {
+      airport.capacity = 2;
+      for (var i = 0; i < 3; i++) {
+        airport.landing();
+      }
+      expect(airport.landing()).toThrow("Hanger full");
+    })
+  })
+
+  describe("#take_off", function() {
+    it("hanger has one less plane when taking off", function() {
+      airport.landing(plane);
+      airport.take_off();
+      expect(airport.hanger.length).toEqual(0);
+    })
+  })
+
+  describe("#take_off", function() {
+    it("plane cannot take off if hanger is empty", function() {
+      expect(airport.take_off()).toThrow("No planes in hanger!");
     })
   })
 })
--- jasmine-standalone-2.8.0/src/airport.js
+++ jasmine-standalone-2.8.0/src/airport.js
@@ -1,7 +1,22 @@
 function Airport() {
   this.hanger = [];
+  this.capacity = 10
 
-  this.landing = function() {
-    this.hanger.push(new Plane);
+  this.landing = function(plane = new Plane) {
+    if (this.hanger.length > this.capacity) {
+      throw "Hanger full";
+    } else {
+      this.hanger.push(plane);
+    }
+  }
+
+  this.take_off = function() {
+    if (this.hanger.length == 0) {
+      throw "No planes in hanger!";
+    } else {
+      this.hanger.pop();
+    }
   }
 }
+
+function Plane() {}
