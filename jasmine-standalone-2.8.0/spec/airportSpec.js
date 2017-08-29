56061efbe1ee3d4b55dbc539f90ef487d926e8dc
pyan83 <puyanwei@gmail.com>
2017-08-29 12:44:27 +0100

    first commit

---
+++ jasmine-standalone-2.8.0/spec/airportSpec.js
@@ -0,0 +1,21 @@
+beforeEach(function() {
+  airport = new Airport();
+})
+
+describe('#Airport', function() {
+  it('Jasmine works', function() {
+    expect().toEqual();
+  })
+})
+//
+// describe('knows when number is NOT', function() {
+//   it('divisible by 3', function() {
+//     expect(fizzbuzz.says(1)).toEqual(1);
+//   })
+// })
+//
+// describe('when playing, says', function() {
+//   it('"Fizzbuzz" when a number is divisible by 15', function() {
+//     expect(fizzbuzz.says(15)).toEqual("Fizzbuzz")
+//   })
+// })
